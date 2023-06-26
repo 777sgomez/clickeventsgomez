3509c09a3b4617b7a2b054bcf575203f1ba0b5a9
Susan Gomez <susangomez@Susans-MacBook-Pro.local>
2023-06-26 12:32:33 -0400

    Added a addEventListener to buttonFoo and added a function for it to clickevents.js

--- clickevents.js
+++ clickevents.js
@@ -1,2 +1,7 @@
 const buttonFoo = document.getElementById("buttonFoo");
 const main = document.querySelector("main");
+buttonFoo.addEventListener('click', reponsetoFoo)
+
+function responsetoFoo() {
+    
+}
