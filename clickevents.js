242b9ab1600d3a1cf4a6f9569b35366ac533a2ab
Susan Gomez <susangomez@Susans-MacBook-Pro.local>
2023-06-26 12:37:44 -0400

    Added a reference to buttonBar with getElementById and also made a addEventListener along with a function to reference responseToBar to clickevents.js

--- clickevents.js
+++ clickevents.js
@@ -7,3 +7,10 @@ function responseToFoo() {
     h3Tag.textContent = "Foo";
     main.appendChild(h3Tag);
 }
+
+const buttonBar = document.getElementById("buttonBar");
+buttonBar.addEventListener('click', responseToBar)
+
+function responseToBar() {
+    
+}
